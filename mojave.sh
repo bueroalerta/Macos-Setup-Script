682eb978e4edd2612db010d1751af1c68c5d228f
Pushkar Singh <theagilecoder@gmail.com>
2018-10-24 22:37:14 +0530

    Added Casks

--- mojave.sh
+++ mojave.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
 
 # Echo command
-echo hello world
+echo Starting Setup...
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Get Cask
+brew tap caskroom/cask
+
+# Install nodejs
+brew install node
+
+# Install casks
+brew cask install google-chrome
+brew cask install opera
+brew cask install mounty
+brew cask install visual-studio-code
+
+# Echo command
+echo Setup Done...
